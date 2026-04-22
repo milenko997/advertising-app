9aa8d7dfe9ae1702d4ca3b39d67a1da0bafadf8d
Milenko Ilic <milenko.ilic997@gmail.com>
2026-04-22 14:53:46 +0200

    Fixing bug

--- config/session.php
+++ config/session.php
@@ -168,7 +168,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', app()->environment('production')),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------

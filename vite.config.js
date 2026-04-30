85a2b10a2eb2bd66ca4819910e3ee1c20a6222ba
Milenko Ilic <milenko.ilic997@gmail.com>
2026-04-30 15:35:14 +0200

    Changing chunk size limit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 1000,
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
